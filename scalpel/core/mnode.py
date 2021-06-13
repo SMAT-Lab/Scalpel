5ec5ff77474bee0b75e382f2cc4cfc258439f9be
Jiawei Wang <jiawei.wang1@monash.edu>
2021-06-14 01:24:50 +1000

    merge core module

---
+++ scalpel/core/mnode.py
@@ -0,0 +1,264 @@
+"""
+In this module, Scalplel provides the interface to users. Each of Python soure
+files are fed into this module to generate an front end object for both parsing
+and code instrumentation. In addition, scope information can also be given for
+fine-grained operations. 
+"""
+import ast
+import os
+import re
+import sys
+import json
+from queue import Queue
+from copy import deepcopy
+from ..core.vars_visitor import get_vars
+from ..core.func_call_visitor import get_func_calls
+from scalpel.core.util import UnitWalker
+
+class ImportRelation:
+
+    def __init__(self):
+        self.path = []
+        self.src : MNode
+        self.dest:MNode
+        self.payload = [] 
+        self.stmts = []
+
+class MNode:
+    """
+    Build a Module node  of the given input source file with publicaly APIs to
+    manipulate for parsing and  code instrumentation
+    file.
+
+    """
+
+    def __init__(self, name):
+        """
+        Args:
+            name: The filename of the input source file.
+        """
+        self.name = name
+        self.full_name = ""
+        self.children = []
+        self.parent = None
+        self.source = ''
+        self.ast = None
+        self.class_pair = None
+        self.node_type_dict = None
+        self.node_type_gt = None
+        self.call_links = None 
+
+    def __str__(self):
+        """
+        returns an string representation of the object
+        """
+        return str(self.name)
+
+    def rewrite(self, scope = "mod"):
+        """
+        rewrite code
+        """
+        pass
+
+    def _read_scope(self, scope):
+        pass
+
+    def parse_vars(self, scope = ""):
+        """
+        Return a list of variable records ranking by their line numbers
+        Args:
+            scope: a dotted string to provide name space. For instance, A.fun
+            means to retreive the function named fun in the class A
+        """
+
+        if scope == "":
+            results = get_vars(self.ast)
+            return results
+        wanted_ast = self._retrieve_by_scope(self.ast, scope)
+        results = get_vars(wanted_ast)
+        return results
+    def gen_import_relations():
+        pass
+
+    def parse_func_calls(self, scope = ""):
+
+        """
+        Return a list of function calls ranking by their line numbers
+        Args:
+            scope: a dotted string to provide name space. For instance, A.fun
+            means to retreive the function named fun in the class A
+        """
+        if scope == "":
+            results = get_vars(self.ast)
+            return results
+        wanted_ast = self._retrieve_by_scope(self.ast, scope)
+        results =  get_func_calls(wanted_ast)
+        return results
+
+    def gen_ast(self):
+        """
+        Build AST tree for th source 
+        """
+        self.ast = ast.parse(self.source)
+
+    def _parse_func_defs(self, ast_node_lst, def_records, scope = "mod"):
+        """
+        Parse the function and class definitions in this module
+        Args:
+            ast_node_lst: a list of statements to be visited.
+            def_records: a list of dictionary, each of whose entry is a
+            function/class definition.
+            scope: the scope that is currently being visited. When it is "mod",
+            it is visiting under the entire module.
+
+        """
+
+        for node in ast_node_lst:
+            def_info = {}
+            if isinstance(node, ast.FunctionDef):
+                def_info = {"scope":scope, "name": node.name, "arg": [], "kws": [],
+                        "lineno":node.lineno, "col_offset":node.col_offset}
+                def_records.append(def_info)
+                self._parse_func_defs(node.body, def_records, scope=node.name)
+
+            elif isinstance(node, ast.ClassDef):
+                #visit class body
+                # to consider class records
+                def_info = {"scope":scope, "name": node.name, "arg": [], "kws": [],
+                        "lineno":node.lineno, "col_offset":node.col_offset}
+                def_records.append(def_info)
+                self._parse_func_defs(node.body, def_records, scope=node.name)
+                pass
+            # assignment statements are useful for assignment graph
+            elif isinstance(node, ast.Assign):
+                pass
+            elif isinstance(node, ast.AugAssign):
+                pass
+            elif isinstance(node, ast.AnnAssign):
+                pass
+
+    def _retrieve_by_scope(self, target_search_node, scope):
+        """
+        retrieve an AST node by the scope directive. 
+        Args:
+            target_search_node: the AST node to be examined for entries in the
+            given scope.
+            function/class definition.
+            scope: a dotted string to provide name space. For instance, A.fun
+            means to retreive the function named fun in the class A
+
+        """
+        if hasattr(target_search_node, "name") and target_search_node.name == scope:
+            return target_search_node
+        for node in target_search_node.body:
+            def_info = {}
+            if isinstance(node, ast.FunctionDef):
+                if node.name == scope:
+                    return node
+                if scope.split('.')[0] == node.name:
+                    return self._retrieve_by_scope(node, scope.lstrip(node.name+'.')) 
+
+            elif isinstance(node, ast.ClassDef):
+                #visit class body
+                # to consider class records
+                if node.name == scope:
+                    return node
+                if scope.split('.')[0] == node.name:
+                    return self._retrieve_by_scope(node, scope.lstrip(node.name+'.'))
+
+            # assignment statements are useful for assignment graph
+            elif isinstance(node, ast.Assign):
+                pass
+            elif isinstance(node, ast.AugAssign):
+                pass
+            elif isinstance(node, ast.AnnAssign):
+                pass
+
+    def parse_func_defs(self):
+        """
+        Return a list of dictionaries, each of its item is a dictionary of
+        function/class definition information.
+        """
+        # mod : module 
+        # func: function
+        def_records = []
+        self._parse_func_defs(self.ast.body, def_records, scope = "mod")
+        return def_records
+
+    def parse_import_stmts(self):
+        """
+        Return a dictionary data structure to map the imported name, from which
+        module and its aliases.
+        """
+        import_stmts = []
+        for stmt in self.ast.body:
+            if isinstance(stmt, (ast.ImportFrom, ast.Import)):
+                import_stmts += [stmt]
+
+        import_dict = {}
+        for stmt in import_stmts:
+            if isinstance(stmt, ast.Import):
+                items = [nn.__dict__ for nn in stmt.names]
+                for d in items:
+                    if d['asname'] is None:  # alias name not found, use its imported name
+                        import_dict[d['name']] = d['name']
+                    else:
+                        import_dict[d['asname']] = d['name'] # otherwise , use alias name
+            if isinstance(stmt, ast.ImportFrom) and stmt.module is not None:
+                # for import from statements
+                # module names are the head of a API name
+                items = [nn.__dict__ for nn in stmt.names]
+                for d in items:
+                    if d['asname'] is None: # alias name not found
+                        import_dict[d['name']] = stmt.module+'.'+d['name']
+                    else:
+                        import_dict[d['asname']] = stmt.module+'.'+d['name']
+        return import_dict
+
+    def retrieve_meta(self, node):
+        results = {"assign_pairs":[], "other_calls":[]}
+        assign_pairs = []
+        other_calls  = []
+        for node in ast.walk(node):
+            if isinstance(node, ast.Assign):
+                call_lst = get_func_calls(node.value)
+                for target in node.targets:
+                    var_info = get_vars(target)[0]
+                    assign_pairs += [{"var":var_info, "calls":call_lst}]
+            elif isinstance(node, ast.AnnAssign):
+                call_lst = get_func_calls(node.value)
+                var_name = get_vars(node.target)[0]
+                assign_pairs += [{"var":var_info, "calls":call_lst}]
+            elif isinstance(node, ast.AugAssign):
+                call_lst = get_func_calls(node.value)
+                var_name = get_vars(node.target)[0]
+                assign_pairs += [{"var":var_info, "calls":call_lst}]
+            else:
+                call_lst = get_func_calls(node)
+                if call_lst not in other_calls:
+                    other_calls.append(call_lst)
+
+        results["assign_pairs"]  = assign_pairs
+        results["other_calls"]  = other_calls
+        return results 
+    def parse_function_body(self):
+        """
+        Prase all function/class definitions
+        """
+        func_records = {}
+        for stmt in self.ast.body:
+            if isinstance(stmt, ast.FunctionDef):
+                func_records[stmt.name] = self.retrieve_meta(stmt)
+            if isinstance(stmt, ast.ClassDef):
+                for c_stmt in stmt.body:
+                    if isinstance(c_stmt, ast.FunctionDef):
+                        func_records[stmt.name+'.' + c_stmt.name] = self.retrieve_meta(c_stmt)
+        return func_records
+
+    def make_unit_walker(self):
+        """
+        Returns a generator of units at statement level
+        """
+        return UnitWalker(self.ast)
+ 
+
---
+++ test-cases/ssa_case-complex.py
@@ -0,0 +1,179 @@
+# Imports
+import os
+import random
+from collections import Counter, defaultdict
+import random
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.chunk import conlltags2tree
+from nltk.tree import Tree
+import pandas as pd
+from htrc_features import FeatureReader
+import geocoder
+import folium
+from pprint import pprint
+from tqdm import tqdm
+# Set environment variable
+# Geonames requires a username to access the API but we do not want to expose personal info in code
+#
+# Run this locally by adding USERNAME to environment variables, e.g. to .env, as follows:
+# > export USERNAME=<insert username here>
+USERNAME = os.getenv('USERNAME')
+# Setup Stanford NER Tagger
+# Ignore deprecation warning for now; we'll deal with it when the time comes!
+st = StanfordNERTagger('/usr/local/share/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz', 
+                       '/usr/local/share/stanford-ner/stanford-ner.jar',
+                       encoding='utf-8')
+# Functions for putting together with inside-outside-beginning (IOB) logic
+# Cf. https://stackoverflow.com/a/30666949
+#
+# For more information on IOB tagging, see https://en.wikipedia.org/wiki/Inside–outside–beginning_(tagging)
+def stanfordNE2BIO(tagged_sent):
+    bio_tagged_sent = []
+    prev_tag = "O"
+    for token, tag in tagged_sent:
+        if tag == "O": #O
+            bio_tagged_sent.append((token, tag))
+            prev_tag = tag
+            continue
+        if tag != "O" and prev_tag == "O": # Begin NE
+            bio_tagged_sent.append((token, "B-"+tag))
+            prev_tag = tag
+        elif prev_tag != "O" and prev_tag == tag: # Inside NE
+            bio_tagged_sent.append((token, "I-"+tag))
+            prev_tag = tag
+        elif prev_tag != "O" and prev_tag != tag: # Adjacent NE
+            bio_tagged_sent.append((token, "B-"+tag))
+            prev_tag = tag
+    return bio_tagged_sent
+def stanfordNE2tree(ne_tagged_sent):
+    bio_tagged_sent = stanfordNE2BIO(ne_tagged_sent)
+    sent_tokens, sent_ne_tags = zip(*bio_tagged_sent)
+    sent_pos_tags = [pos for token, pos in pos_tag(sent_tokens)]
+    sent_conlltags = [(token, pos, ne) for token, pos, ne in zip(sent_tokens, sent_pos_tags, sent_ne_tags)]
+    ne_tree = conlltags2tree(sent_conlltags)
+    return ne_tree
+# Sample HathiTrust ID
+# This is the HTID for... 
+# "Ancient Corinth: A guide to the excavations," O. Broneer, R. Carpenter, and C. H. Morgan
+htid = "wu.89079728994"
+# Get HTEF data for this ID; specifically tokenlist
+fr = FeatureReader(ids=[htid])
+for vol in fr:
+    tokens = vol.tokenlist()
+# Create pandas dataframe with relevant data
+temp = tokens.index.values.tolist()
+counts = pd.DataFrame.from_records(temp, columns=['page', 'section', 'token', 'pos'])
+counts['count'] = tokens['count'].tolist()
+counts[:10]
+# Reconstruct text using tokens and counts
+text_data = list(zip(counts['token'].tolist(), counts['count'].tolist()))
+# Loop through and multiply words by counts
+text_list = []
+for w, c in text_data:
+    for i in range(0, c):
+        text_list.append(w)
+random.shuffle(text_list) # Necessary?
+text_reconstruction = " ".join(text_list)
+#page_words_extended = page_words+page_ner
+tokens = word_tokenize(text_reconstruction)
+tagged_tokens = st.tag(tokens)
+tagged_tokens = [item for item in tagged_tokens if item[0] != '']
+ne_tree = stanfordNE2tree(tagged_tokens)
+ne_in_sent = []
+for subtree in ne_tree:
+    if type(subtree) == Tree: # If subtree is a noun chunk, i.e. NE != "O"
+        ne_label = subtree.label()
+        ne_string = " ".join([token for token, pos in subtree.leaves()])
+        ne_in_sent.append((ne_string, ne_label))
+    
+locations = [tag[0].title() for tag in ne_in_sent if tag[1] == 'LOCATION']
+print(locations)
+most_common_locations = Counter(locations).most_common(10)
+pprint(most_common_locations)
+# Organize some data for map info
+places_list = [name for name, _ in most_common_locations][:3] # Limit to top three
+most_common_locations = dict(most_common_locations) # Turn mcl into dictionary
+# Retrieve json from geonames API (for fun this time using geocoder)
+geocoder_results = []
+for place in places_list:
+    results = geocoder.geonames(place, maxRows=5, key=USERNAME)
+    jsons = []
+    for result in results:
+        jsons.append(result.json)
+    geocoder_results.append(jsons)
+# Create a list of 'country' from the geonames json results
+countries = []
+for results in geocoder_results:
+    for item in results:
+        if 'country' in item.keys():
+            countries.append(item['country'])
+# Determine which country appears most often
+top_country = sorted(Counter(countries))[0]
+print(top_country)
+# Iterate over geocoder_results and keep the first lat/long that matches the top country
+coordinates = []
+for i, results in enumerate(geocoder_results):
+    for item in results:
+        if item['country'] == top_country:
+            coordinates.append((float(item['lat']), float(item['lng'])))
+            break # Only get the first item for now
+print(places_list)            
+print(coordinates)
+# Set up Folium and populate with weighted coordinates
+basemap = folium.Map(location=[37.97945, 23.71622], zoom_start=8, tiles='cartodbpositron', width=960, height=512)
+for i, c in enumerate(coordinates):
+    folium.CircleMarker([c[0], c[1]], radius=most_common_locations[places_list[i]]*.25, color='#3186cc',
+                    fill=True, fill_opacity=0.5, fill_color='#3186cc', 
+                    popup='{} ({}, {}) appears {} times in book.'.format(places_list[i], c[0], c[1], most_common_locations[places_list[i]])).add_to(basemap)
+print('Map of relevant locations in Broneer et al.\'s "Ancient Corinth: A guide to the excavations," weighted by frequency.')
+basemap
+page = 87
+test = counts[counts['page'] == page]['token'].tolist()
+print(test)
+print(len(test))
+from nltk.corpus import stopwords
+stops = set(stopwords.words('english'))
+pns_list = []
+for i in range(1, max(counts['page'])+1):
+    tokens = counts[counts['page'] == i]['token'].tolist()
+    tokens = [token for token in tokens if token.lower() not in stops and len(token) > 2]
+    pns = [token for token in tokens if token[0].isupper()]
+    combs = [f'{x} {y}' for x, y in combinations(pns, 2)]
+    pns_list.extend(combs)
+print([x for x, y in Counter(pns_list).most_common(25)])
+geocoder_results = []
+for place in pns_list[:15]:
+    results = geocoder.geonames(place, maxRows=5, key=USERNAME)
+    jsons = []
+    for result in results:
+        jsons.append(result.json)
+    geocoder_results.append(jsons)
+geocoder_results
+results = geocoder.geonames('Roman Forum', maxRows=5, key=USERNAME)
+print(next(result.address for result in results))
+g = geocoder.google('Al-Fayum')
+print(g.latlng)
+coordinates = [g.latlng]
+# Set up Folium and populate with weighted coordinates
+basemap = folium.Map(location=[37.97945, 23.71622], zoom_start=8, tiles='cartodbpositron', width=960, height=512)
+for i, c in enumerate(coordinates):
+    folium.CircleMarker([c[0], c[1]], radius=most_common_locations[places_list[i]]*.25, color='#3186cc',
+                    fill=True, fill_opacity=0.5, fill_color='#3186cc').add_to(basemap)
+basemap
+test = [x for x, y in Counter(pns_list).most_common(10)]
+places = []
+coordinates = []
+for item in test:
+    g = geocoder.google(item)
+    if g:
+        places.append(g.address)
+        print(g.address)
+        coordinates.append(g.latlng)
+# Set up Folium and populate with weighted coordinates
+basemap = folium.Map(location=[37.97945, 23.71622], zoom_start=8, tiles='cartodbpositron', width=960, height=512)
+for i, c in enumerate(coordinates):
+    folium.CircleMarker([c[0], c[1]], color='#3186cc',
+                    fill=True, fill_opacity=0.5, fill_color='#3186cc').add_to(basemap)
+basemap        
