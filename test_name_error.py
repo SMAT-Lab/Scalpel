200879938dff2bdef2cb88ca84203c568b4b70c7
Jiawei Wang <jiawei.wang1@monash.edu>
2021-07-15 14:44:07 +1000

    save test work on name error

--- test_name_error.py
+++ test_name_error.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import ast
 import astor
@@ -11,7 +12,8 @@ def do_single_notebook(filename):
     try:
         with open(filename) as f:
             nb = nbformat.read(f, as_version=4)
-            cells = list(filter(lambda x:x['cell_type'] == 'code' , nb.cells))
+            cells = list(filter(lambda x:x['cell_type'] == 'code' and
+                x['execution_count'] is not None, nb.cells))
             nb.cells = cells # assign new cells
             source_cells = [c['source'] for c in nb.cells]
             source_all = "\n".join(source_cells)
@@ -30,7 +32,15 @@ def do_single_notebook(filename):
         print(e)
         return None
 
-def test_SSA(source):
+def get_name_from_msg(msg):
+    patter = r"/'((?:''|[^'])*)'/"
+    idx2 = msg.find("is not defined")
+    groups = re.findall(r"\'(.+?)\'", msg[idx2-30:idx2])
+    if len(groups)>0:
+        return groups[-1]
+    return None
+
+def test_SSA(source, target_ident):
     mnode = MNode("local")
     mnode.source = source
     mnode.gen_ast()
@@ -38,23 +48,17 @@ def test_SSA(source):
     cfg = mnode.gen_cfg()
     m_ssa = SSA(source)
     m_ssa.compute_SSA(cfg)
-
     m_final_idents = m_ssa.compute_final_idents()
-
     live_ident_table = [m_final_idents]
-    s = m_ssa.test()
-    if s == False:
-        print('Yes!!!')
-
+    undefined_idents = m_ssa.test()
     #print(m_final_idents)
-    #cfg.build_visual('cfg', 'pdf')
+    graph_viz = cfg.build_visual('pdf')
+    #graph_viz.render("cfg.pdf", view=True)
     for fun_name, fun_cfg in cfg.functioncfgs.items():
         fun_ssa = SSA(source)
         fun_ssa.compute_SSA(fun_cfg)
         fun_ssa.test(live_ident_table=live_ident_table)
-        s = fun_ssa.test(live_ident_table=live_ident_table)
-        if s == False:
-            print('Yes!!!')
+        undefined_idents += fun_ssa.test(live_ident_table=live_ident_table)
 
     for class_name, class_cfg in cfg.class_cfgs.items():
         # class body ssa compute
@@ -68,26 +72,41 @@ def test_SSA(source):
             #    inside_fun_cfg.build_visual('cfg', 'pdf')
             fun_ssa = SSA(source)
             fun_ssa.compute_SSA(inside_fun_cfg)
-            s = fun_ssa.test(live_ident_table=live_ident_table)
-            print(s)
-            if s == False:
-                print('Yes!!!')
-            #print(class_name, inside_fun_name)
-            #inside_fun_cfg.build_visual('cfg', 'pdf')
-            #break
+            undefined_idents += fun_ssa.test(live_ident_table=live_ident_table)
         live_ident_table.pop()
+    print(target_ident)
+    print(undefined_idents)
+    if target_ident in undefined_idents:
+        return True
+    return False
+
+def test_SSA_single():
+    fn = "fminkin@ML.py" 
+    src_path = os.path.join('nameerror-tests', fn)
+    exec_log_path = os.path.join("sniffer-dog-exp-data/base/exec_log/", fn.split('.py')[0])
+    msg = open(exec_log_path).read()
+    name = get_name_from_msg(msg)
+    src = open(src_path).read()
+    res = test_SSA(src, name)
 
 def test_SSA_batch():
     #filename = sys.argv[1]
     #source = open(filename).read()
-    all_fns = os.listdir('tmp')
+    all_fns = os.listdir('nameerror-tests')
     for fn in all_fns:
-        if fn!='prabhasyadav@Env_Modeling.py':
-            continue
-        src_path = os.path.join('tmp', fn)
+        #if fn!='djahng@sentiment-rnn.py':
+        #    continue
+        src_path = os.path.join('nameerror-tests', fn)
+        exec_log_path = os.path.join("sniffer-dog-exp-data/base/exec_log/", fn.split('.py')[0])
+        msg = open(exec_log_path).read()
+        name = get_name_from_msg(msg)
+        #src_path ='tmp.py'
         src = open(src_path).read()
-        print(src)
-        test_SSA(src)
+        #print(src)
+        #print(fn)
+        res = test_SSA(src, name)
+        if not res:
+            print(fn)
         #break
 
 def main():
@@ -112,4 +131,5 @@ def main():
     return 0
 if __name__ == '__main__':
     #main()
-    test_SSA_batch()
+    #test_SSA_batch()
+    test_SSA_single()
