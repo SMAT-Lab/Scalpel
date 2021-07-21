a5b0f50cd9e7a40f16f8107b5a06374d2cae04cd
Jiawei Wang <jiawei.wang1@monash.edu>
2021-07-21 20:22:05 +1000

    add utility file

---
+++ scalpel/util.py
@@ -0,0 +1,10 @@
+#scan a folder recurisively and return all files ending with the flag
+def get_path_by_ext(root_dir, flag='.py'):
+    paths = []
+    for root, dirs, files in os.walk(root_dir):
+        files = [f for f in files if not f[0] == '.']  # skip hidden files such as git files
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+        for f in files:
+            if f.endswith(flag):
+                paths.append(os.path.join(root, f))
+    return paths
