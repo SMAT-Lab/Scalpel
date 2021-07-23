201f63ac72102dbbd1ed810ed1edb8aa506723cc
Jiawei Wang <jiawei.wang1@monash.edu>
2021-07-23 23:48:42 +1000

    update test name error script

--- test_name_error.py
+++ test_name_error.py
@@ -41,72 +41,115 @@ def get_name_from_msg(msg):
     return None
 
 def test_SSA(source, target_ident):
+
     mnode = MNode("local")
     mnode.source = source
     mnode.gen_ast()
     ast_node = mnode.ast
+    if ast_node is None: 
+        print('syntax')
+        return False
+
     cfg = mnode.gen_cfg()
     m_ssa = SSA(source)
     m_ssa.compute_SSA(cfg)
     m_final_idents = m_ssa.compute_final_idents()
     live_ident_table = [m_final_idents]
     undefined_idents = m_ssa.test()
-    #print(m_final_idents)
     graph_viz = cfg.build_visual('pdf')
     #graph_viz.render("cfg.pdf", view=True)
+    def_names = []
+
     for fun_name, fun_cfg in cfg.functioncfgs.items():
+        args = cfg.function_args[fun_name]
+        arg_idents = {arg_name:[2] for arg_name in args}
+        live_ident_table.append(arg_idents)
         fun_ssa = SSA(source)
         fun_ssa.compute_SSA(fun_cfg)
-        fun_ssa.test(live_ident_table=live_ident_table)
-        undefined_idents += fun_ssa.test(live_ident_table=live_ident_table)
+        undefined_idents += fun_ssa.test(live_ident_table=live_ident_table, def_names = def_names)
+        live_ident_table.pop()
+        def_names += [fun_name]
 
+    cfg.class_cfgs = {}
     for class_name, class_cfg in cfg.class_cfgs.items():
         # class body ssa compute
         c_ssa = SSA(source)
         c_ssa.compute_SSA(class_cfg)
         c_final_idents = c_ssa.compute_final_idents()
-        #print(c_final_idents)
         live_ident_table.append(c_final_idents)
         for inside_fun_name, inside_fun_cfg in class_cfg.functioncfgs.items():
-            #if inside_fun_name == 'status_printer':
-            #    inside_fun_cfg.build_visual('cfg', 'pdf')
+            args = class_cfg.function_args[inside_fun_name]
+            arg_idents = {arg_name:[2] for arg_name in args}
+            live_ident_table.append(arg_idents)
             fun_ssa = SSA(source)
             fun_ssa.compute_SSA(inside_fun_cfg)
-            undefined_idents += fun_ssa.test(live_ident_table=live_ident_table)
+            undefined_idents += fun_ssa.test(live_ident_table=live_ident_table, def_names = def_names)
+            live_ident_table.pop()
+            def_names += [inside_fun_name]
+        def_names = def_names[0:-len(class_cfg.functioncfgs.items())]
+
         live_ident_table.pop()
+
+    print('--------------------------')
     print(target_ident)
     print(undefined_idents)
+    print('--------------------------')
+
     if target_ident in undefined_idents:
         return True
     return False
 
 def test_SSA_single():
-    fn = "fminkin@ML.py" 
-    src_path = os.path.join('nameerror-tests', fn)
+    #fn = "TilakD@Time-Series-Prediction-and-Text-Generation---RNN.py" # case
+    #passed
+    #fn = "whugue@clickstream-analysis.py"  # syntax error
+    #fn = "howl-anderson@q_learning_demo.py"  # lib issues
+    #fn = "TermiNutZ@pills_online.py"
+    #fn = "jlewi@kubeflow-rl.py"
+    fn = "arm61@isistrainingcourse_mar2018.py"  # syntax
+    #fn = "tamanyan@ml-baseball.py"  # wrong !!
+    #fn = "bigspider@arclimb.py"   # order
+    #fn = "pysal@geopyter.py" # syntax
+    #fn = "KirstieJane@BrainsForPublication.py" # syntax
+    #fn = "Devjiu@diploma.py" # syntax
+    #fn = "Neurita@nipype-lessons.py" # syntax
+    #fn = "chrisjcc@DataInsight.py" # R script syntax
+    #fn = "independent_example.py"
+
+    src_path = os.path.join('test-cases', 'name-error', fn)
     exec_log_path = os.path.join("sniffer-dog-exp-data/base/exec_log/", fn.split('.py')[0])
     msg = open(exec_log_path).read()
     name = get_name_from_msg(msg)
+    #name = "Activation"
     src = open(src_path).read()
+    try:
+        tree = ast.parse(src)
+    except Exception as e:
+        os.system('2to3 '+ src_path + ' -n -W  --output-dir=tmp/')
+        src = open('tmp/'+fn).read()
     res = test_SSA(src, name)
+    print(res)
 
 def test_SSA_batch():
     #filename = sys.argv[1]
     #source = open(filename).read()
-    all_fns = os.listdir('nameerror-tests')
+    all_fns = os.listdir('name-error-cases')
     for fn in all_fns:
         #if fn!='djahng@sentiment-rnn.py':
         #    continue
-        src_path = os.path.join('nameerror-tests', fn)
+        src_path = os.path.join('name-error-cases', fn) 
         exec_log_path = os.path.join("sniffer-dog-exp-data/base/exec_log/", fn.split('.py')[0])
         msg = open(exec_log_path).read()
         name = get_name_from_msg(msg)
         #src_path ='tmp.py'
         src = open(src_path).read()
-        #print(src)
-        #print(fn)
         res = test_SSA(src, name)
-        if not res:
+        if  res:
+           # print('Yes')
+            pass
+        else:
             print(fn)
+           # print("No")
         #break
 
 def main():
@@ -116,20 +159,22 @@ def main():
         line_parts = row.strip().split('|')
         filename = line_parts[-1]
         repo_name = line_parts[1]
-        log_dir = os.path.join("/data/sda/jiawei/sniffer-dog-exp-data/base/exec_log/", repo_name)
-        content = open(log_dir).read()
-        print(repo_name)
-        print(content[0:300])
+        #log_dir = os.path.join("/data/sda/jiawei/sniffer-dog-exp-data/base/exec_log/", repo_name)
+        #content = open(log_dir).read()
+        #print(repo_name)
+        #print(content[0:300])
         #print(filename)
         #filename = "/mnt/fit-Knowledgezoo/Github_repos_download/data/diyclassics@mapping-experiments/book-map-ner-htef.ipynb"
-        #s = do_single_notebook(filename)
-        #f = open('tmp/'+line_parts[1]+ '.py', 'w')
-        #f.write(s)
-        #f.close() 
+        filename = os.path.basename(filename)
+        notebook_path = os.path.join("name-error-notebooks", repo_name+'-'+ filename)
+        s = do_single_notebook(notebook_path)
+        f = open('nameerror-tests/'+repo_name+ '.py', 'w')
+        f.write(s)
+        f.close() 
         #break
         #break
     return 0
 if __name__ == '__main__':
-    #main()
+    main()
     #test_SSA_batch()
-    test_SSA_single()
+    #test_SSA_single()
