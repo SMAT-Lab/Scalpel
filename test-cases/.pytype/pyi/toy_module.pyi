1ad1e268a799483ed7e8b57702da0c23ea397735
Jiawei Wang <jiawei.wang1@monash.edu>
2021-08-03 19:06:21 +1000

    sync

---
+++ test-cases/.pytype/pyi/toy_module.pyi
@@ -0,0 +1,8 @@
+# (generated with --quick)
+
+a: int
+obj: A
+s: int
+
+class A:
+    def fun(self) -> int: ...
--- test-cases/type.py
+++
@@ -1,8 +0,0 @@
-def func(a:int, b:int)->str:
-    if True:
-        a = str(a)
-    else:
-        a = 10
-    return a
-
-res = func(10,20)
