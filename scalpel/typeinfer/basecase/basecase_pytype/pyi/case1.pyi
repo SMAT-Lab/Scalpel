afbc8aa4bc7c430c4f9e80c5fa1f821625b21175
nitram <17760090+mden29@users.noreply.github.com>
2021-09-19 12:03:20 +1000

    adding pytype stub files

---
+++ scalpel/typeinfer/basecase/basecase_pytype/pyi/case1.pyi
@@ -0,0 +1,8 @@
+# (generated with --quick)
+
+import collections
+from typing import Dict, Type
+
+defaultdict: Type[collections.defaultdict]
+
+def my_function() -> Dict[str, str]: ...
---
+++ scalpel/typeinfer/basecase/basecase_pytype/pyi/case8.pyi
@@ -0,0 +1,5 @@
+# (generated with --quick)
+
+from typing import Any, Callable
+
+def my_function() -> Callable[[], Any]: ...
