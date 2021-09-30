e077def6f6bd807f1b65b99a9647222be8c619dc
Louis Tran <lstrn98@gmail.com>
2021-09-30 19:21:05 +1000

    Add base case for Brian. Update readme
    Not a heuristic, just added feature

---
+++ scalpel/typeinfer/basecase/case26.py
@@ -0,0 +1,14 @@
+"""
+For bryan
+var ass map - tuple assignments
+"""
+
+
+def test():
+    class ObjRand:
+        pass
+    x = (ObjRand(), ObjRand())
+    x = ('a', 'b', 'c')
+    y = None
+    for i in x:
+        y = i
