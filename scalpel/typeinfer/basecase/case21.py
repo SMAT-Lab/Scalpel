0ff8ec514b0a8e9006105b9d72d90381d3e3a80e
nitram <17760090+mden29@users.noreply.github.com>
2021-09-30 22:51:46 +1000

    heuristic 2 refactor - supports multiple args and is cleaner now hopefully

--- scalpel/typeinfer/basecase/case21.py
+++ scalpel/typeinfer/basecase/case21.py
@@ -4,20 +4,21 @@
 # case21.py: x -> str
 
 def func_3():
-    func_1("test")
-    func_1(func_2())
+    func_1("test", 20)
+    func_1(func_2(), func_4())
     return True
 
 
-def func_1(my_parameter):
+def func_1(my_parameter, parameter_two):
     # do stuff
     result = my_parameter
-    return result
+    thing = parameter_two
+    return result, thing
 
 
 def func_2():
-    return 20
+    return "test"
 
 
-
-
+def func_4():
+    return 30
