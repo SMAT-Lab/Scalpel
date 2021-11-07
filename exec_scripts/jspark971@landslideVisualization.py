1dd4fc1ca079fb02dd84ace1300d3ef13c3da0ec
Jiawei Wang <jiawei.wang1@monash.edu>
2021-11-08 00:11:56 +1100

    remove a folder

--- exec_scripts/jspark971@landslideVisualization.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# In[1]:
-import pandas as pd 
-dataset = pd.read_csv("global_landslides.csv")
-# In[2]:
-dataset.head()
--- exec_scripts/saturnist@datavisualisation.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# In[6]:
-import parse as parse
-import graph as graph
-# In[7]:
