aae512ac62d8602ff4de762564612be16ef4bc12
billquan <haowei.quan8@gmail.com>
2021-11-16 01:07:47 +1100

    upload type-infer user guide

---
+++ examples/type_infer_example.py
@@ -0,0 +1,6 @@
+from os import getcwd
+
+
+def my_function():
+    x = "Current working directory: "
+    return x + getcwd()
---
+++ examples/type_infer_tutorial.py
@@ -0,0 +1,5 @@
+from scalpel.typeinfer.typeinfer import TypeInference
+
+inferer = TypeInference(name='type_infer_example.py', entry_point='type_infer_example.py')
+inferer.infer_types()
+inferred = inferer.get_types()
