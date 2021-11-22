8c7460381e041cb398fcfbc9c740ec3fa389180d
Jiawei Wang <jiawei.wang1@monash.edu>
2021-11-22 16:50:00 +1100

    upload rewriting example

---
+++ examples/rewriter_example.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import ast
+import astor
+
+from scalpel.rewriter import Rewriter
+
+src = """
+a = list()
+b = dict()
+"""
+expected_src = """
+a = []
+b = {}
+"""
+
+def rewrite_rules(node)->list:
+
+    if  isinstance(node, ast.Assign):
+      if isinstance(node.value, ast.Call) and hasattr(node.value.func, "id"):
+          if node.value.func.id == "list":
+              new_assign_value = ast.List(elts=[], ctx=ast.Load())
+              new_stmt = ast.Assign(node.targets, new_assign_value)
+              return [new_stmt]
+          if node.value.func.id == "dict":
+              new_assign_value = ast.Dict(keys=[], values=[])
+              new_stmt = ast.Assign(node.targets, new_assign_value)
+              return [new_stmt]            
+def main():
+    rewriter = Rewriter()
+    new_src = rewriter.rewrite(src, rule_func = rewrite_rules)
+    print(new_src)
+
+if __name__ == '__main__':
+    main()
+
