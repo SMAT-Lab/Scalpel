fba0bc6ca38292578ec6b513d59614f462a39db0
billquan <haowei.quan8@gmail.com>
2021-11-27 03:01:38 +1100

    set up setup.py

--- examples/cg_tutorial.py
+++ examples/cg_tutorial.py
@@ -1,5 +1,5 @@
 from scalpel.pycg.pycg import CallGraphGenerator
 
-cg_generator = CallGraphGenerator(["cg_example_pkg/main.py"], "cg_example_pkg")
+cg_generator = CallGraphGenerator(["./cg_example_pkg/main.py"], "cg_example_pkg")
 cg_generator.analyze()
 cg = cg_generator.output
