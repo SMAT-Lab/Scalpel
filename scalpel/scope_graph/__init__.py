6103572307e38e005a0bfab45eec3853c90b05d8
Jiawei Wang <jw_jw@outlook.com>
2022-04-22 19:27:25 +1000

    add reference of the paper 'A theory of Name Resolution'

---
+++ scalpel/scope_graph/__init__.py
@@ -0,0 +1,6 @@
+"""
+Neron, P., Tolmach, A., Visser, E., Wachsmuth, G. (2015). A Theory of Name Resolution. In: Vitek, J. (eds) Programming Languages and Systems. 
+ESOP 2015. Lecture Notes in Computer Science(), vol 9032. Springer, Berlin, Heidelberg. https://doi.org/10.1007/978-3-662-46669-8_9
+
+https://link.springer.com/content/pdf/10.1007/978-3-662-46669-8_9.pdf
+"""
