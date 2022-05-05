d87b45fd0d474ed7463c6963c3d7d24c39839acd
billquan <haowei.quan8@gmail.com>
2022-05-05 19:27:34 +1000

    fix pycg version compatibility issue

--- scalpel/call_graph/pycg.py
+++ scalpel/call_graph/pycg.py
@@ -6,5 +6,16 @@ This file is a wrapper of the pycg, a practical python call graph generator. Ple
 Call Graph Generation in Python. In 43rd International Conference on Software Engineering, ICSE '21, 25–28 May 2021.
 """
 
-from pycg.pycg import CallGraphGenerator
+from pycg.pycg import CallGraphGenerator as CallGraphGeneratorPyCG
 from pycg import formats
+import pycg
+import pkg_resources
+import packaging
+from packaging import version
+pycg_version = pkg_resources.get_distribution('pycg').version
+if packaging.version.Version(pycg_version)>packaging.version.Version("0.0.3"):
+    class CallGraphGenerator(CallGraphGeneratorPyCG):
+        def __init__(self, entry_points, package, max_iter=-1, operation="call-graph"):
+            super().__init__(entry_points, package, max_iter, operation)
+
+    pycg.pycg.CallGraphGeneratorPyCG = CallGraphGenerator
