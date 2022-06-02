1ebe2d1ed5d1180594c1073b1910940339d06b96
billquan <haowei.quan8@gmail.com>
2022-06-02 23:57:02 +1000

    restore the examples

--- examples/import_graph_tutorial.py
+++ examples/import_graph_tutorial.py
@@ -1,12 +1,9 @@
-import os
 from scalpel.import_graph.import_graph import Tree,ImportGraph
 
-
-path_to_current_file = os.path.realpath(__file__)
-current_directory = os.path.split(path_to_current_file)[0]
-root_node = Tree("import_graph_example_pkg")
-import_graph = ImportGraph(os.path.join(current_directory,"import_graph_example_pkg"))
+root_node = Tree("./import_graph_example_pkg")
+import_graph = ImportGraph("./import_graph_example_pkg")
 import_graph.build_dir_tree()
 module_dict = import_graph.parse_import(root_node)
+print(module_dict)
 
 
