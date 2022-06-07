4ab24042f03f28144dfc3353e8c33a0d5ea0f095
Jiawei Wang <jw_jw@outlook.com>
2022-06-07 21:17:12 +1000

    rewrite the example code for import graph

--- examples/import_graph_tutorial.py
+++ examples/import_graph_tutorial.py
@@ -1,9 +1,16 @@
 from scalpel.import_graph.import_graph import Tree,ImportGraph
 
-root_node = Tree("./import_graph_example_pkg")
-import_graph = ImportGraph("./import_graph_example_pkg")
-import_graph.build_dir_tree()
-module_dict = import_graph.parse_import(root_node)
-print(module_dict)
+
+def main():
+
+    target_dir = "./import_graph_example_pkg"
+    import_graph = ImportGraph(target_dir)
+    import_graph.build_dir_tree()
+    all_leaf_ndoes = import_graph.get_leaf_nodes()
+    for node in all_leaf_ndoes:
+        module_dict = import_graph.parse_import(node.ast)
+        print(module_dict)
 
 
+if __name__ == "__main__":
+    main() 
