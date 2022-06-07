1cc5ac003650b231e8f44566119c849121e2d404
Jiawei Wang <jw_jw@outlook.com>
2022-06-07 21:08:54 +1000

    add a test script for the robustness of the CFG module

--- tests/test_cfg_robust.py
+++ tests/test_cfg_robust.py
@@ -1,55 +1,43 @@
+'''
+The objective of this script is to process as many files as possible to test the robustness of the framework. 
+
+'''
+
 from scalpel.cfg.builder import CFGBuilder
+from scalpel.cfg.model import Block
+from scalpel.util import get_path_by_ext
 import os
 import sys
-import glob
+import glob 
 
 
+#@profile
 def test_all():
+    target_dir = "./cfg_robust_cases/"
+    all_files =  get_path_by_ext(target_dir)
+    
+    print(f"In total, there are {len(all_files)} files to be tested!")
+    for fn in all_files:
+        builder = CFGBuilder()
+        print(os.path.basename(fn), fn)
+        cfg = builder.build_from_file(os.path.basename(fn), fn)
+     
+    return 0
 
-    os_projects_path = "tests/test-cases/cfg-tests/"
-    except_no = 0
-    file_no = 0
-    error_log = "error_log_cfg_robust.txt"
-
-    files = glob.glob(os_projects_path + '*.py', recursive=True)
-    for file_path in files:
-        print(file_path)
-        file_no += 1
-        try:
-            builder = CFGBuilder()
-            cfg = builder.build_from_file("test", file_path)
-            for i, block in enumerate(cfg):
-                value = cfg.get_return_value(block)
-                line_no = block.at()
-                # calls = block.get_calls()
-            graph = cfg.build_visual("pdf")
-        except Exception as e:
-            print(e)
-            with open(error_log, 'a') as f:
-                f.write("File: {} ".format(file_path))
-                f.write(str(e))
-                f.write("\n")
-            except_no += 1
-
+    
+    
+         
     print("Number of py files: "+str(file_no))
     print("Number of exception: "+str(except_no))
 
 
 def main():
-    src_file = sys.argv[1]
-    src = open(src_file).read()
-    builder = CFGBuilder()
-    cfg = builder.build_from_file("test", src_file)
-    for i, block in enumerate(cfg):
-        value = cfg.get_return_value(block)
-        line_no = block.at()
-        calls = block.get_calls()
-    #graph = cfg.build_visual("pdf")
-    #graph.render("cfg-test", view=False)
+    test_all()
+    #src = open(src_file).read()
+    #builder = CFGBuilder()
+    #cfg = builder.build_from_file("test", src_file)
 
 
 if __name__ == '__main__':
-    test_all()
-
-
+    main() 
 
