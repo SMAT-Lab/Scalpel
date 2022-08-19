43886b39fc30871b7a64de7fd5f9d3549ce3fc63
tristanlatr <tris.la.tr@gmail.com>
2022-08-19 19:41:30 -0400

    Enable viewcode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx.ext.autodoc', 
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
