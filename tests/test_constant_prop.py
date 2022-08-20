1f4fc7a502d5bc2eb0cb5e5cfe22879b90a259a1
simisimon <Bastisimon95@googlemail.com>
2022-08-20 11:24:07 +0200

    Get values for tuples

--- tests/test_constant_prop.py
+++ tests/test_constant_prop.py
@@ -1,1 +1,35 @@
-#  test  if pasing contants to function call params
+'''
+The objective of this script is to check if the constant propagation module is able to detect all possible values of variables. 
+'''
+
+from scalpel.cfg.builder import CFGBuilder
+from scalpel.SSA.const import SSA
+import ast
+
+def test_tuples():
+    target_file = "tests/test-cases/constant_propagation/tuples.py"
+    cfg = CFGBuilder().build_from_file(name="callable", filepath=target_file)
+    ssa = SSA()
+    
+    _, const_dict = ssa.compute_SSA(cfg)
+
+    tuple_var_a = next(filter(lambda x: "a" in x[0][0], const_dict.items()))
+    tuple_var_b = next(filter(lambda x: "b" in x[0][0], const_dict.items()))
+    tuple_var_c = next(filter(lambda x: "c" in x[0][0], const_dict.items()))
+    tuple_var_d = next(filter(lambda x: "d" in x[0][0], const_dict.items()))
+
+    assert tuple_var_a[1]
+    assert isinstance(tuple_var_a[1], ast.Constant)
+    assert tuple_var_b[1]
+    assert isinstance(tuple_var_b[1], ast.Constant)
+    assert tuple_var_c[1]
+    assert isinstance(tuple_var_c[1], ast.Call)
+    assert tuple_var_d[1]
+    assert isinstance(tuple_var_d[1], ast.Call)
+
+
+def main():
+    test_tuples()
+
+if __name__ == '__main__':
+    main() 
