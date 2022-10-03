5dbd85f8e8e73fa8b51f56110a81b13502ec5dc5
Jiawei Wang <jw_jw@outlook.com>
2022-10-03 15:45:47 +1100

    add docstring

--- scalpel/dataflow/IFDS.py
+++ scalpel/dataflow/IFDS.py
@@ -1,15 +1,21 @@
 import networkx as nx 
 
+
 class IFDS():
-    # call-node c and return-site
+    """
+    There are four different flow functions
+    1. Normal： propgates intra-procedural flows along non-call nodes
+    2. Call： propagates from caller  to callee
+    3. Return：propagates from callee to caller
+    4. Call-to-return: propagates intra-procedural flows over call nodes
+       # most often is simply the identity function.
+    source: IFDS on-the-fly algorithm by Prof. Eric Bodden. https://www.youtube.com/watch?v=beE3zPnxvkE&t=745s
 
-    # for every 
-
+    """
     def __init__(self) -> None:
         self.domains = set()
         self.i_cfgs = {}  # flattened 
         self.super_graph = nx.DiGraph()
-
         pass
 
     def call_to_return_site(self):
