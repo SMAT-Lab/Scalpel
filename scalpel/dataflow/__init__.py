8272df4fb94d1a71a2c97bf68439ba70f120bfb3
Jiawei Wang <jw_jw@outlook.com>
2022-10-03 14:53:40 +1100

    This the first commit for the IFDS implementation

---
+++ scalpel/dataflow/__init__.py
@@ -0,0 +1,5 @@
+"""
+In this module, we implement the IFDS framework our Scalpel projoect. Please see the following references:
+
+"Precise Interprocedural Dataflow Analysis via Graph Reachability", POPL 1995
+"""
