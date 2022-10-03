392cfd4ea47f4ecec10bdc5b55b1082a1743fa72
Jiawei Wang <jw_jw@outlook.com>
2022-10-03 15:27:13 +1100

    add class body

---
+++ scalpel/dataflow/IFDS.py
@@ -0,0 +1,22 @@
+import networkx as nx 
+
+class IFDS():
+    # call-node c and return-site
+
+    # for every 
+
+    def __init__(self) -> None:
+        self.domains = set()
+        self.i_cfgs = {}  # flattened 
+        self.super_graph = nx.DiGraph()
+
+        pass
+
+    def call_to_return_site(self):
+        pass 
+    
+    def call_to_start(self):
+        pass 
+    
+    def exit_to_return_site(self):
+        pass 
