67c27e674e5bbd56040a0157c8e553de6cdcce4f
Jiawei Wang <jw_jw@outlook.com>
2022-12-24 01:10:25 +1100

    remove dataflow from main. This will be merged when development is done

--- scalpel/dataflow/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-In this module, we implement the IFDS framework our Scalpel projoect. Please see the following references:
-
-"Precise Interprocedural Dataflow Analysis via Graph Reachability", POPL 1995
-"""
