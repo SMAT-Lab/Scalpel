64d24cb4af23a5f391f528788104c947e699c39c
Ashwin Prasad <ashwinprasad.me@gmail.com>
2023-03-31 11:05:26 +0200

    Refactoring Codebase: - requirements.txt freeze - vscode config - pre-commit configs for linting

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
-from setuptools import setup, find_packages
-setup( 
-    name = "python-scalpel",
-    version = "1.0beta",
-    description = "Scalpel: The Python Program Analysis Framework",
-    author = "Jiawei Wang and Li Li and Haowei Quan",
-    author_email = "jiawei.wang1@monash.edu, li.li@monash.edu, haowei.quan@monash.edu",
-    url = "https://github.com/SMAT-Lab/Scalpel",
-    packages= find_packages(include=['scalpel', 'scalpel.*']),
-    install_requires = [
-        'astor~=0.8.1',
-        'graphviz~=0.17',
-        'networkx',
-        'astunparse~=1.6.3',
-        'typed-ast',
-        'typeshed-client',
-        'regex',
-        'setuptools',
-        'dataclasses',
-        'pycg',
-        ],
-    extras_require = {
-        'docs':
-            [
-            'sphinx', 
-            'myst_parser', 
-            'sphinx_rtd_theme', 
-            'sphinxcontrib-spelling', 
-            'pydoctor', 
-            ]
-        },
-    )
+# Principles from: https://blog.miguelgrinberg.com/post/the-package-dependency-blues
+from setuptools import find_packages, setup
 
+setup(
+    name="python-scalpel",
+    version="1.0beta",
+    description="Scalpel: The Python Program Analysis Framework",
+    author="Jiawei Wang and Li Li and Haowei Quan",
+    author_email="jiawei.wang1@monash.edu, li.li@monash.edu, haowei.quan@monash.edu",
+    url="https://github.com/SMAT-Lab/Scalpel",
+    packages=find_packages(include=["scalpel", "scalpel.*"]),
+    install_requires=[
+        "astor~=0.8.1",
+        "graphviz~=0.17",
+        "networkx",
+        "astunparse~=1.6.3",
+        "typed-ast~=1.5.4",
+        "typeshed-client~=2.2.0",
+        "regex",
+        "setuptools",
+        "dataclasses",
+        "pycg~=0.0.6",
+    ],
+    extras_require={
+        "docs": [
+            "sphinx",
+            "myst_parser",
+            "sphinx_rtd_theme",
+            "sphinxcontrib-spelling",
+            "pydoctor",
+        ]
+    },
+)
