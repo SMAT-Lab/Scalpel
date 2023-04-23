cdd72b021a712ba81aef4027f44ffbcba12251f2
Jiawei Wang <jw_jw@outlook.com>
2023-04-24 00:14:49 +1000

    change the description of the project

--- src/scalpel/__init__.py
+++ src/scalpel/__init__.py
@@ -1,11 +1,11 @@
 """
-Static Anaysis for Python Programs
+Scalpel: Static Anaysis for Python Programs
 ==================================
 Scalpel is a Python library integrating classical program anaysis algorithms
-with tailored features for Python language. It aims to provide simple and
-efficient solutions to software engineering researchers that are accessible to
+with tailored features for Python language. It aims to provide simple and efficient solutions to software engineering researchers that are accessible to
 everybody and reusable in various contexts.
+For more information, please see Scalpel: [The Python Static Analysis Framework](https://github.com/SMAT-Lab/Scalpel)
 """
 
-__all__ = ["cfg", "call_graph", "SSA", "core", "typeinfer", "import_graph", "rewriter"]
+__all__ = ["cfg", "call_graph", "SSA", "core", "typeinfer", "import_graph", "rewriter", "file_system"]
 __version__ = "1.0dev"
