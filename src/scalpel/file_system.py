3a6163c1d2f5fb66a1cf48176796de27ccb18153
Jiawei Wang <jw_jw@outlook.com>
2023-04-26 00:27:32 +1000

    add parent name for each node and add util function to handle relative import path

--- src/scalpel/file_system.py
+++ src/scalpel/file_system.py
@@ -4,6 +4,7 @@ The purpose of this componement is to support modelling large Python project str
 This component is still under development.
 
 Author: Jiawei Wang
+
 """
 
 import os
@@ -19,12 +20,13 @@ class Node:
     Module name, absolute path name, AST and source string are recorded in this dataclass.
     In addition, fields of its parent and childs are recorded. 
     """
-    def __init__(self, name, abs_path, mod_src, mod_ast, mode_type="module"):
+    def __init__(self, name, abs_path, mod_src, mod_ast, parent = None,  node_type="module"):
         self.name:str = name   # module name 
         self.abs_path = abs_path
         self.mod_src = mod_src
         self.mod_ast = mod_ast
-        self.mod_type =  mode_type  #   there are two different node:  module and package
+        self.node_type =  node_type  #   there are two different node:  module and package
+        self.parent = parent
         
     def __str__(self):
         return str(self.name)
@@ -65,7 +67,7 @@ class FileSystem:
             mod_name = fn 
             mod_src = open(abs_path).read()
             mod_ast = ast.parse(mod_src)
-            mod_node = Node(mod_name, abs_path, mod_src, mod_ast)
+            mod_node = Node(mod_name, abs_path, mod_src, mod_ast, node_type="module")
             self.all_mod_nodes.append(mod_node)
         else:
             for root, dirs, files in os.walk(self.entry_point):
@@ -73,23 +75,24 @@ class FileSystem:
                     f for f in files if not f[0] == "."
                 ]  # skip hidden files such as git files
                 dirs[:] = [d for d in dirs if not d[0] == "."]
+
                 for f in files:
                     if f.endswith(self.file_ext):    
                         abs_path = os.path.join(root, f)
                         mod_name = f 
                         mod_src = open(abs_path).read()
                         mod_ast = ast.parse(mod_src)
-                        mod_node = Node(mod_name, abs_path, mod_src, mod_ast)
+                        self.parse_import(mod_ast)
+                        mod_node = Node(mod_name, abs_path, mod_src, mod_ast, parent=root, node_type="module")
                         self.all_mod_nodes.append(mod_node)
                 #return 0
-                
+
                 for d in dirs:
-                    
                     abs_path = os.path.join(root, d)
                     mod_name = d 
                     mod_src = None
                     mod_ast = None 
-                    mod_node = Node(mod_name,abs_path, mod_src, mod_ast)
+                    mod_node = Node(mod_name,abs_path, mod_src, mod_ast, parent=root, node_type="package")
                     self.all_mod_nodes.append(mod_node)
                     
 
@@ -102,74 +105,44 @@ class FileSystem:
         Returns:  a list of Nodes  
         """
 
-        return self.all_mod_nodes
+        return [node for node in self.all_mod_nodes if node.node_type == "module"]
     
-    def format_all_imports(self, import_statement):
-        '''
-        TODO:
-        To convert all import path such as from ..X import fun as  its abs import path.
-        
-        '''
+    def get_subpackages(self) -> List[Node]:
+        return [node for node in self.all_mod_nodes if node.node_type == "package"]
+    
+    
+    def _meta_data():
+        """
+        To parse each of modules and extract meta information.
+        """
         pass 
-        
-    '''
+   
+    
+    def format_import_path(self, import_stmt, abs_path):
+        abs_import_path = []
+        if isinstance(import_stmt, ast.Import):
+            for alias in import_stmt.names:
+                abs_import_path.append(alias.name)
+            pass 
+        elif isinstance(import_stmt, ast.ImportFrom):
+            import_path_parts = abs_path.split(".")
+            abs_import_path + [import_path_parts[:-import_stmt.level] + "." + import_stmt.module]
+            pass 
+        else:
+            raise ("unknown input argument")
 
-    def go_to_that_node(self, cur_node, visit_path):
-        """
-        To locate a particular node from the tree from the current node given a visit path from import statement. 
-        For instance,  a visit path of [example, module_a, func] means, if we can locate the function definition `func` from module_a under example folder. 
-        The function tries to locate the given path using both relative and absolute import path. 
-        Args:
-        visit_path: a list of names that represent different level python modules.
-        """
-        route_length = len(visit_path)
-        tmp_node = None
-        if route_length == 0:
-            return self.root
-        # Go to the siblings of the current node
-        # This is the topmost node
-        if cur_node.parent is None:
-            return tmp_node
-
-        tmp_node = self.find_node_by_name(cur_node.parent.children, visit_path[0])
-        if tmp_node is not None:
-            for i in range(1, route_length):
-                tmp_node = self.find_node_by_name(tmp_node.children, visit_path[i])
-                if tmp_node is None:
-                    break
-        elif visit_path[0] == self.root.name:
-            # From the topmost, this rule for Pandas or django
-            tmp_node = self.root
-            for i in range(1, route_length):
-                tmp_node = self.find_node_by_name(tmp_node.children, visit_path[i])
-                if tmp_node is None:
-                    break
-            return tmp_node
-        elif visit_path[0] == cur_node.parent.name:
-            # From its parent
-            tmp_node = cur_node.parent
-            for i in range(1, route_length):
-                tmp_node = self.find_node_by_name(tmp_node.children, visit_path[i])
-                if tmp_node is None:
-                    break
-        # we are still in the directory
-        if tmp_node is not None and tmp_node.name.endswith('.py') is not True:
-            tmp_node = self.find_node_by_name(tmp_node.children, '__init__.py')
-        return tmp_node
-        
-    '''
-    @staticmethod
-    def parse_import(tree):
+    def parse_import(self, tree)->dict:
         """
         To parse import statements from the AST tree
         Args:
-        tree: Python AST object
-        Returns: an import map data structure
+        tree: Python import statement
+        Returns: mport path
         """
         module_item_dict = {}
         try:
             for node in ast.walk(tree):
-                if isinstance(node, ast.ImportFrom):
+                if isinstance(node, (ast.ImportFrom, ast.Import)):
+                    self.format_import_path(node)
                     if node.module is None and node.level not in module_item_dict:
                         module_item_dict[node.level] = []
                     elif node.module not in module_item_dict:
@@ -184,22 +157,6 @@ class FileSystem:
             return module_item_dict
         except AttributeError:
             return None
-
-    @staticmethod
-    def extract_class_from_source(source):
-        """
-        To parse import statements from the AST tree
-        Args:
-        source: source code text
-        Returns: class/function definitions, ast tree and alias pairs 
-        """
-        tree = ast.parse(source, mode='exec')
-        visitor = SourceVisitor()
-        visitor.visit(tree)
-     
-        return visitor.result, tree, visitor.pair
-    
-   
 
     @staticmethod
     def find_child_by_name(node, name):
@@ -222,14 +179,14 @@ class FileSystem:
         return None
 
 def main():
-    ep  = sys.argv[1]
-
+    ep  = sys.argv[1]  # entry point 
     fs = FileSystem(ep)
 
     fs.build_dir_tree()
     all_leaf_node = fs.get_leaf_nodes()
+    all_package_node = fs.get_subpackages()
 
 
-    print(len(all_leaf_node))
+    print(len(all_leaf_node), len(all_package_node))
 if __name__ == "__main__":
     main()
