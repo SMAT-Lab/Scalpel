e5274f54374baa9b747e6a538b40481c1766afd1
Jiawei Wang <jiawei.wang1@monash.edu>
2023-09-15 00:01:29 +1000

    add some doc string

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,3 +1,6 @@
+"""
+This is the test suite for the def-use compoenent for scalpel. All test cases at the moment are from beniget project.
+"""
 import tests.test_definitions
 import tests.test_chains
 import tests.test_capture
