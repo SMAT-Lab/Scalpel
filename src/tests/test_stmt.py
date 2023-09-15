5cc73eb9db98da17f94b97ffc01515e429b20a67
Craze-H <70187423+Craze-H@users.noreply.github.com>
2023-09-15 14:26:47 +0800

    fix function args error

---
+++ src/tests/test_stmt.py
@@ -0,0 +1,24 @@
+import unittest, ast, astor
+from ..scalpel.core.base.stmt import AssignStmt, SStmt
+from _ast import AST, Assign, SetComp
+# from duc import _ContainerRelationshipVisitor
+
+class TestStmtMethods(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_AssignStmt_get_clauses(self):
+        assign_node = ast.parse("a = b = 1").body[0]
+        print(type(assign_node))
+        self.assertIsInstance(assign_node, ast.stmt)
+        sstmt = SStmt(assign_node)
+        print(sstmt.get_clauses())
+        # self.assertEqual(sstmt.get_clauses().value, 1)
+        
+
+if __name__ == '__main__':
+    unittest.main()
