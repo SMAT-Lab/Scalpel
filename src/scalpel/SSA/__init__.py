ebf403546d74df3ea568e21d4dcc60c963fa49ca
Hervé Beraud <hberaud@redhat.com>
2024-03-14 10:15:45 +0100

    [trivial] fix typo

--- src/scalpel/SSA/__init__.py
+++ src/scalpel/SSA/__init__.py
@@ -1,5 +1,5 @@
 """
-Static Single Assignment (SSA) is a technique of IR in the compiling thoery, it also shows great benefits to static anaysis tasks such as constant propagation, dead code elimination and etc.
+Static Single Assignment (SSA) is a technique of IR in the compiling theory, it also shows great benefits to static anaysis tasks such as constant propagation, dead code elimination and etc.
 Constant propagation is also a matured technique in static anaysis.
 It is the process of evaluating or recognizing the actual constant values or expressions at a particular program point. This is realized by utilizing control flow and data flow information. Determining the possible values for variables before runtime gives great benefits to software anaysis.
 For instance, with constant value propagation, we can detect and remove dead code or perfrom type checking.
